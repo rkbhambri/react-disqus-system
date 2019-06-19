5d72d113f0c9416ee4e49e940922ba2b2563d108
ram <ramkrishan@kontikilabs.com>
2019-06-19 17:01:05 +0530

    Updates

---
+++ precache-manifest.f69e79680b8fe7283baa75a1d272074a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d0356f606ef6208077e638924331d5c",
+    "url": "/react-disqus-system/static/media/like.9d0356f6.svg"
+  },
+  {
+    "revision": "a0eb004449fabd2b2c55564ed68f5a8b",
+    "url": "/react-disqus-system/static/media/dislike.a0eb0044.svg"
+  },
+  {
+    "revision": "27ebeb2fab68ed24b36818b374ae70aa",
+    "url": "/react-disqus-system/static/media/anonymous.27ebeb2f.jpg"
+  },
+  {
+    "revision": "51cf6924340a1033d317",
+    "url": "/react-disqus-system/static/js/runtime~main.7ff58e3d.js"
+  },
+  {
+    "revision": "eccca3778efddfbc0a33",
+    "url": "/react-disqus-system/static/js/main.9a8d0675.chunk.js"
+  },
+  {
+    "revision": "05eeb1f0373197031db5",
+    "url": "/react-disqus-system/static/js/2.6baf0a3a.chunk.js"
+  },
+  {
+    "revision": "eccca3778efddfbc0a33",
+    "url": "/react-disqus-system/static/css/main.baa23b14.chunk.css"
+  },
+  {
+    "revision": "05eeb1f0373197031db5",
+    "url": "/react-disqus-system/static/css/2.f651e8ed.chunk.css"
+  },
+  {
+    "revision": "482a4ebb1f7e024f8b4e411512f80d91",
+    "url": "/react-disqus-system/index.html"
+  }
+];
